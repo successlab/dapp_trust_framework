cfd4d5252f701b2247531779a573fb5367a2fb67
Suraj S Jain <surajsjain@hotmail.com>
2022-05-26 20:07:42 -0500

    Code cleaning - Moved the page operations

---
+++ pageops/content_ops.py
@@ -0,0 +1,52 @@
+import requests
+from bs4 import BeautifulSoup
+
+
+def get_github_search_results_page(contract_address, github_cookie):
+    cookies = github_cookie
+
+    headers = {
+        'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10.15; rv:100.0) Gecko/20100101 Firefox/100.0',
+        'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,*/*;q=0.8',
+        'Accept-Language': 'en-US,en;q=0.5',
+        'DNT': '1',
+        'Connection': 'keep-alive',
+        'Upgrade-Insecure-Requests': '1',
+        'Sec-Fetch-Dest': 'document',
+        'Sec-Fetch-Mode': 'navigate',
+        'Sec-Fetch-Site': 'none',
+        'Sec-Fetch-User': '?1',
+        'If-None-Match': 'W/"9fef31759f86dcc19c79727867089080"',
+    }
+
+    params = {
+        'q': contract_address,
+        'type': 'code',
+    }
+
+    response = requests.get('https://github.com/search', params=params, cookies=cookies, headers=headers)
+
+    return response.content
+
+
+def usage_stats(search_results_content):
+    response_soup = BeautifulSoup(search_results_content, 'html.parser')
+
+    languages_and_counts_raw = response_soup.find("div", class_="border rounded-2 p-3 mb-3 d-none d-md-block") \
+        .find_all("a", class_="filter-item")
+
+    lang_counts = {}
+
+    for raw_ele in languages_and_counts_raw:
+        content_eles = raw_ele.text.split()
+        lang_name = ''.join(content_eles[1:])
+        lang_files_count = int(content_eles[0])
+        lang_counts[lang_name] = lang_files_count
+
+    return lang_counts
+
+
+def get_max_pagecount(search_results_content):
+    response_soup = BeautifulSoup(search_results_content, 'html.parser')
+    paginator = response_soup.find("div", class_="paginate-container codesearch-pagination-container")
+    return int(paginator.find_all("a")[-2].text)
