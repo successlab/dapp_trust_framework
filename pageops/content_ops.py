552be96e857783f5b7694de510009c68c6b5c0f2
Suraj S Jain <surajsjain@hotmail.com>
2022-05-27 11:55:53 -0500

    Contract links fetching

--- pageops/content_ops.py
+++ pageops/content_ops.py
@@ -22,3 +22,21 @@ def get_max_pagecount(search_results_content):
     response_soup = BeautifulSoup(search_results_content, 'html.parser')
     paginator = response_soup.find("div", class_="paginate-container codesearch-pagination-container")
     return int(paginator.find_all("a")[-2].text)
+
+
+def get_contract_users_and_repos(search_results_content, users_and_repos_mapping={}):
+    response_soup = BeautifulSoup(search_results_content, 'html.parser')
+
+    code_search_results = response_soup.find("div", class_="col-12 col-md-9 float-left px-2 pt-3 pt-md-0 codesearch-results")
+    users_and_repos_raw = code_search_results.find_all("a", class_="Link--secondary")
+
+    for user_and_repo in users_and_repos_raw:
+        user, repo = user_and_repo.text.split("/")
+        user = user.strip()
+        repo = repo.strip()
+        if user in users_and_repos_mapping:
+            users_and_repos_mapping[user].append(repo)
+        else:
+            users_and_repos_mapping[user] = [repo]
+
+    return users_and_repos_mapping
