8cefea208cd8057007207f5ca666bb6dec3763c6
Suraj S Jain <surajsjain@hotmail.com>
2022-07-04 15:19:33 -0500

    Added default contract address

--- utils/test_github_crawler.py
+++ utils/test_github_crawler.py
@@ -8,7 +8,13 @@ from utils.crawler_ops.fetching.search_results import get_github_search_results_
 
 
 if __name__ == '__main__':
-    contract_address = sys.argv[1]
+    try:
+        contract_address = sys.argv[1]
+    except:
+        # Default contract: UniswapV3Staker (1.0.2)
+        contract_address = "0xe34139463bA50bD61336E0c446Bd8C0867c6fE65"
+        print("No contract address provided,\n"
+              "Using the default contract address as: ")
 
     search_results_content = get_github_search_results_page(contract_address, github_cookie)
     lang_counts = usage_stats(search_results_content)
