d30ef77a5d027ec2cf00effb99f3c2323ef309d2
Suraj S Jain <surajsjain@hotmail.com>
2022-07-06 21:46:38 -0500

    Test for github crawling

--- contract_relations/tests.py
+++ contract_relations/tests.py
@@ -1,3 +1,9 @@
 from django.test import TestCase
+from utils.github_crawler import get_github_search_results, write_search_results_into_db
 
 # Create your tests here.
+class TestGithubSearch(TestCase):
+    def test_search(self):
+        search_address = "0xe34139463bA50bD61336E0c446Bd8C0867c6fE65"
+        res = get_github_search_results(search_address)
+        write_search_results_into_db(res, search_address)
