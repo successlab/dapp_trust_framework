705a54cc6f16a7ec4043f90d93ecd58d70ab3476
Suraj S Jain <surajsjain@hotmail.com>
2022-08-20 13:50:30 -0500

    Tools from ContractFuzzer to analyse the signature

--- utils/contract_bin_ops/get_function_signature_from_abi.py
+++ utils/contract_bin_ops/get_function_signature_from_abi.py
@@ -1,15 +1,20 @@
+import argparse
 import os
 import re
-import  sys
-import  argparse
+import sys
+
 import demjson
-import  hashlib
-if sys.version_info <(3,6):
-    import  sha3
+
+if sys.version_info < (3, 6):
+    import sha3
+
+
 def getFiles(path):
     files = os.listdir(path)
     return files
     pass
+
+
 def getRawContractName(file_name):
     pattern = re.compile(r'\s*(\S+)\..*')
     m = pattern.match(file_name)
@@ -18,69 +23,84 @@ def getRawContractName(file_name):
         return name
     return None
     pass
+
+
 def getAbi(file_path):
-    with open(file_path,"r") as f:
+    with open(file_path, "r") as f:
         data = f.read()
         f.close()
-        abi = demjson.decode(data,"utf-8")
+        abi = demjson.decode(data, "utf-8")
         return abi
+
+
 def getFuns(abi):
     funs = list()
     for ele in abi:
         if "type" in ele and ele["type"] == "function":
-           funs.append(ele)
+            funs.append(ele)
     return funs
     pass
+
+
 def getFunSig(fun):
     if "name" not in fun:
         return None
     name = fun["name"]
     if "inputs" not in fun:
-        return name+"()"
+        return name + "()"
     else:
         inputs = fun["inputs"]
         types = list()
         for input in inputs:
             types.append(input["type"])
         typesStr = ",".join(types)
-        return name+"("+typesStr+")"
+        return name + "(" + typesStr + ")"
     pass
+
+
 def getFunId(Sig):
     # hashlib.s?
     s = sha3.keccak_256()
     s.update(Sig.encode("utf8"))
     hex = s.hexdigest()
-    bytes4 = "0x"+hex[:8]
+    bytes4 = "0x" + hex[:8]
     return bytes4
     pass
+
+
 def test():
     sig = "approve(address,uint256)"
     print(getFunId(sig))
     pass
-def solve_file(abi_dir,abi_item):
+
+
+def solve_file(abi_dir, abi_item):
     funid_path = r"./abi_sig/"
     if not os.path.exists(funid_path):
         os.mkdir(funid_path)
     f = open(os.path.join(os.path.abspath(funid_path), abi_item), "w+")
     abi = getAbi(os.path.join(os.path.abspath(abi_dir), abi_item))
     if abi:
-            funs = getFuns(abi)
-            if funs:
-                for fun in funs:
-                    sig = getFunSig(fun)
-                    id = getFunId(sig)
-                    f.write(":".join([id, sig]))
-                    f.write("\n")
+        funs = getFuns(abi)
+        if funs:
+            for fun in funs:
+                sig = getFunSig(fun)
+                id = getFunId(sig)
+                f.write(":".join([id, sig]))
+                f.write("\n")
     f.close()
     pass
+
+
 def solve_dir(abi_dir):
     dirs = os.listdir(abi_dir)
     for item in dirs:
         try:
-            solve_file(abi_dir,item)
+            solve_file(abi_dir, item)
         except:
             continue
     pass
+
 
 def main():
     global args
@@ -97,19 +117,19 @@ def main():
                           help="set contracts' abis dir from where to get the #sols.abis and #sols specified by -c or by -a")
     args = parser.parse_args()
     if args.contract:
-       if args.contract.find("."):
-           args.contract = args.contract.split(".")[0]+".abis"
+        if args.contract.find("."):
+            args.contract = args.contract.split(".")[0] + ".abis"
     if args.abi_dir:
         if args.abi_dir[-1] == "/":
-            args.abi_dir = args.abi_dir[:len(args.abi_dir)-1]
+            args.abi_dir = args.abi_dir[:len(args.abi_dir) - 1]
     if not args.all:
-        solve_file(args.abi_dir,args.contract)
+        solve_file(args.abi_dir, args.contract)
     else:
         solve_dir(args.abi_dir)
     pass
 
 
-if __name__=="__main__":
+if __name__ == "__main__":
     # main()
     test()
     pass
