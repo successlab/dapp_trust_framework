4807fbf4abe59b87078cec88d3796a732054ce95
Suraj S Jain <surajsjain@hotmail.com>
2022-09-11 17:10:31 -0500

    Function for extracting ABI only from etherscan

--- utils/etherscan_ops/contract_extractor.py
+++ utils/etherscan_ops/contract_extractor.py
@@ -2,11 +2,11 @@ import requests
 import json
 
 
-def get_contract_props_from_etherscan(address):
-    ABI_ENDPOINT = (
+def get_all_contract_props(address):
+    api_endpoint = (
         "https://api.etherscan.io/api?module=contract&action=getsourcecode&address="
     )
-    response = requests.get(ABI_ENDPOINT + address)
+    response = requests.get(api_endpoint + address)
     response_json = response.json()
 
     source_code = response_json["SourceCode"]
@@ -14,3 +14,14 @@ def get_contract_props_from_etherscan(address):
     is_proxy = response_json["Proxy"] == "1"
 
     return source_code, abi, is_proxy
+
+
+def get_contract_abi(address):
+    api_endpoint = (
+        "https://api.etherscan.io/api?module=contract&action=getsourcecode&address="
+    )
+    response = requests.get(api_endpoint + address)
+    response_json = response.json()
+    abi = json.loads(response_json["result"])
+
+    return abi
