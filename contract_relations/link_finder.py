0e4dcd7dd547e25fb2f1480877119524b298c8be
Suraj S Jain <surajsjain@hotmail.com>
2022-10-31 02:13:43 -0500

    Code for finding the attribute-based links

--- contract_relations/link_finder.py
+++ contract_relations/link_finder.py
@@ -1,16 +1,16 @@
 from utils.bytecode_analyzers.bytecode_to_opcode import get_opcode
-from utils.bytecode_analyzers.opcode_analyzer import get_external_addresses
+from utils.bytecode_analyzers.opcode_analyzer import get_external_addresses_in_code
+from utils.bytecode_analyzers.stored_address_vals import get_storage_locations, get_addresses_in_storage
 from utils.extractors.geth_node_extractor import get_contract_bin
-from utils.extractors.etherscan_extractor import get_contract_abi
 
 def get_code_links(address):
     bytecode = str(get_contract_bin(address).hex())
     opcode_instructions = get_opcode(bytecode)
-    code_link_addresses = get_external_addresses(opcode_instructions)
+    code_link_addresses = get_external_addresses_in_code(opcode_instructions)
     return list(code_link_addresses)
 
 def get_attribute_links(address):
-    abi = get_contract_abi(address)
-
-    #TODO: Use the public variables/attributes with addresses
-    pass
+    bytecode = str(get_contract_bin(address).hex())
+    instructions = get_opcode(bytecode)
+    storage_locations = get_storage_locations(instructions)
+    return get_addresses_in_storage(storage_locations)
