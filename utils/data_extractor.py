7bf31ff0dc19ba9e269d7d27a5054ba2f95e8538
Suraj S Jain <30551982+surajsjain@users.noreply.github.com>
2023-01-29 21:10:17 -0600

    Extraction from 21st chunk

--- utils/data_extractor.py
+++ utils/data_extractor.py
@@ -104,7 +104,7 @@ def write_into_dataset(in_csv_path, out_csvs_dir_path, data_length=1586, chunk_s
     ]
 
     limit = int(ceil(data_length/chunk_size))
-    for i in range(1, limit):
+    for i in range(21, limit):
         print("Processing Chunk " + str(i) + " at time: " + str(datetime.now()))
         if i == 0:
             df = pd.read_csv(in_csv_path, skiprows=(i * chunk_size), nrows=chunk_size, names=base_df_col_names, header=None)
