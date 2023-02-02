81506c35d50bdb14e53447747dd486b085bfeedf
Suraj S Jain <surajsjain@hotmail.com>
2023-02-02 14:09:28 -0600

    Trust score API

---
+++ trust_scoring/apis.py
@@ -0,0 +1,32 @@
+from rest_framework.views import APIView
+from rest_framework.permissions import AllowAny
+from rest_framework.response import Response
+
+from utils.trust_scoring.data_persistance import write_features_df_into_db
+from utils.trust_scoring.feature_extractor import get_features_df
+from utils.trust_scoring.ml_model_runner import get_prob_trust_score
+
+
+class GetTrustScore(APIView):
+	permission_classes = [AllowAny]
+
+	def get(self, request):
+		in_data = request.data
+		address = in_data["address"]
+
+		contract_attribs_df = get_features_df(address)
+
+		prob_score = get_prob_trust_score(contract_attribs_df)
+
+		write_features_df_into_db(
+			address,
+			contract_attribs_df,
+			trust_score=prob_score,
+		)
+
+		response_dict = {
+			"trust_score": prob_score,
+			"contract_attributes": contract_attribs_df.iloc[0].to_dict(),
+		}
+
+		return Response(response_dict)
--- trust_scoring/urls.py
+++ trust_scoring/urls.py
@@ -1,3 +1,6 @@
 from django.urls import path
+from .apis import GetTrustScore
 
-urlpatterns = []
+urlpatterns = [
+	path("get_trust_score/", GetTrustScore.as_view(), name="get_trust_score"),
+]
