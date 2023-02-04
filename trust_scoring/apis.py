5e46d95c0cc24eb0582ada77fcbd54189f58b414
Suraj S Jain <surajsjain@hotmail.com>
2023-02-04 16:03:10 -0600

    EOA score unavailable response

--- trust_scoring/apis.py
+++ trust_scoring/apis.py
@@ -3,6 +3,7 @@ from rest_framework.permissions import AllowAny
 from rest_framework.response import Response
 
 from trust_scoring.db_writer import generate_and_store_score
+from utils.basic_web3.address_classifier import is_contract
 
 
 class GetTrustScore(APIView):
@@ -10,6 +11,10 @@ class GetTrustScore(APIView):
 
     def get(self, request):
         address = request.query_params.get("address")
+
+        if not is_contract(address):
+            return Response({"status": "Failed", "Message": "The address provided is not a smart contract"})
+
         response_dict = generate_and_store_score(address)
 
         return Response(response_dict)
