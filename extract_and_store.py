d06826b34b5aaab609ee3cc4925dd69e68ce0dac
Suraj S Jain <surajsjain@hotmail.com>
2023-02-07 01:59:41 -0600

    Extract and store

---
+++ extract_and_store.py
@@ -0,0 +1,33 @@
+import subprocess
+import json
+
+
+def run_securify(address, api_key_path):
+	cmd = f"sudo docker run -it -v /home/web3success/securify2:/share securify {address} --from-blockchain --key {api_key_path}"
+	output = subprocess.check_output(cmd, shell=True).decode()
+	return output
+
+
+def main(addresses, api_key_path):
+	with open('tool_output.csv', 'w') as f:
+		f.write("Address,tool_output\n")
+		for address in addresses:
+			result = run_securify(address, api_key_path)
+			result_dict = {"output": result}
+			f.write(address + "," + result_dict + '\n')
+			print("Finished ", address)
+
+
+def read_address_list():
+	with open("address_list.txt", "r") as f:
+		raw_str = f.read()
+
+	addresses = raw_str.split("\n")
+	return addresses
+
+
+if __name__ == '__main__':
+	addresses = read_address_list()
+	api_key_path = '/share/api_key.txt'
+	main(addresses, api_key_path)
+
