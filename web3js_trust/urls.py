9a9178fddb26d7f0bf5e13c92385bacaeac5c21a
Suraj S Jain <surajsjain@hotmail.com>
2023-02-25 20:12:13 -0600

    APIs for extracting ABI availability

--- web3js_trust/urls.py
+++ web3js_trust/urls.py
@@ -5,4 +5,5 @@ from .apis import *
 urlpatterns = [
     path('web3js_stats/', CheckWeb3JSStats.as_view(), name="webjs_stats"),
     path('extract_data/', ExtractData.as_view(), name="extract_data"),
+    path('abi_availability/', CheckABIAvailability.as_view(), name="abi-availability"),
 ]
